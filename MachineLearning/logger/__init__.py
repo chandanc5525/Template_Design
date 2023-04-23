a4d7b6cd3e4508a66d379000192a1f0938407dc1
chaudhari.chandan22@gmail.com <chaudhari.chandan22@gmail.com>
2023-04-24 02:47:51 +0530

    logging file completed

---
+++ MachineLearning/logger/__init__.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+import logging
+import os, sys   
+# os - Allows us to run a command in the Python script
+# sys - The python sys module provides functions and variables which are used to manipulate different parts of the Python Runtime Environment. It lets us access system-specific parameters and functions
+
+LOG_DIR = "logs"
+
+CURRENT_TIME_STEMP = f"log{datetime.now().strftime('%Y-%m-%d %H-%M-%S')}"
+
+# year, Month, Day, Hours, Mintue, Sec
+
+log_file_name = f"logs{CURRENT_TIME_STEMP}.log"
+
+os.makedirs(LOG_DIR, exist_ok=True)
+
+log_file_path = os.path.join(LOG_DIR, log_file_name)
+
+logging.basicConfig(filename = log_file_path,
+                    filemode="w",
+                    format = '[%(asctime)s] %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO
+                    )
